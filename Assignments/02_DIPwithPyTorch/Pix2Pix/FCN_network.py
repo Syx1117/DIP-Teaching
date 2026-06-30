ae05012e7f4f44319eeceab03002bda55bfd2480
Syx1117 <161568043+Syx1117@users.noreply.github.com>
2026-06-30 13:53:48 +0800

    Update FCN_network.py

--- Assignments/02_DIPwithPyTorch/Pix2Pix/FCN_network.py
+++ Assignments/02_DIPwithPyTorch/Pix2Pix/FCN_network.py
@@ -1,29 +1,81 @@
+import torch
 import torch.nn as nn
+
 
 class FullyConvNetwork(nn.Module):
 
     def __init__(self):
         super().__init__()
-         # Encoder (Convolutional Layers)
+        # Encoder (Convolutional Layers)
         self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 8, kernel_size=4, stride=2, padding=1),  # Input channels: 3, Output channels: 8
-            nn.BatchNorm2d(8),
-            nn.ReLU(inplace=True)
+            nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
         )
-        ### FILL: add more CONV Layers
-        
-        # Decoder (Deconvolutional Layers)
-        ### FILL: add ConvTranspose Layers
-        ### None: since last layer outputs RGB channels, may need specific activation function
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+        )
+
+        self.deconv1 = nn.Sequential(
+            nn.ConvTranspose2d(512, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+        )
+        self.deconv2 = nn.Sequential(
+            nn.ConvTranspose2d(1024, 256, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+        )
+        self.deconv3 = nn.Sequential(
+            nn.ConvTranspose2d(512, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+        )
+        self.deconv4 = nn.Sequential(
+            nn.ConvTranspose2d(256, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+        )
+        self.deconv5 = nn.Sequential(
+            nn.ConvTranspose2d(128, 3, kernel_size=4, stride=2, padding=1),
+            nn.Tanh(),
+        )
 
     def forward(self, x):
-        # Encoder forward pass
-        
-        # Decoder forward pass
-        
-        ### FILL: encoder-decoder forward pass
+        enc1 = self.conv1(x)
+        enc2 = self.conv2(enc1)
+        enc3 = self.conv3(enc2)
+        enc4 = self.conv4(enc3)
+        enc5 = self.conv5(enc4)
 
-        output = ...
-        
+        dec1 = self.deconv1(enc5)
+        dec1 = torch.cat([dec1, enc4], dim=1)
+
+        dec2 = self.deconv2(dec1)
+        dec2 = torch.cat([dec2, enc3], dim=1)
+
+        dec3 = self.deconv3(dec2)
+        dec3 = torch.cat([dec3, enc2], dim=1)
+
+        dec4 = self.deconv4(dec3)
+        dec4 = torch.cat([dec4, enc1], dim=1)
+
+        output = self.deconv5(dec4)
         return output
-    
